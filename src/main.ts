727247756f10684b309e0668447fd35cd79c6331
younghoonKimm <choosey_@naver.com>
2022-01-06 19:01:58 +0900

    [add] install swagger, price api

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import { ValidationPipe } from "@nestjs/common";
 import "reflect-metadata";
 
 import { AppModule } from "./app.module";
+import { setupSwagger } from "./middlewares/setupSwaggr";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3000;
-
+  setupSwagger(app);
   await app.listen(port);
 }
 bootstrap();
