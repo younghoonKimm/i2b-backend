2a818a2ed22ec3ab7e2c6fda06fa1baf2fce7b4d
younghoonKimm <choosey_@naver.com>
2022-01-17 15:12:49 +0900

    [add] alluser dto

--- src/admin/dto/admin-info.dto.ts
+++ src/admin/dto/admin-info.dto.ts
@@ -1,63 +1,25 @@
 import { Column } from "typeorm";
 import { IsString, Length, IsEmail, IsEnum } from "class-validator";
-import { ApiProperty, PickType } from "@nestjs/swagger";
+import { ApiProperty, PickType, PartialType } from "@nestjs/swagger";
 import { CommonOutPut } from "src/common/dto/common.dto";
-import { AdminRole } from "../entities/admin-info.entity";
+import { AdminRole, AdminInfoEntity } from "../entities/admin-info.entity";
 
-export class AdminCreateInputDto {
-  @IsString()
-  @ApiProperty({
-    example: "system",
-  })
-  adminId: string;
-
-  @IsString()
-  @Length(13, 30)
-  @ApiProperty({
-    example: "olivestonelab12##",
-  })
-  adminPw: string;
-
-  @IsString()
-  @ApiProperty({
-    example: "system",
-  })
-  adminName: string;
-
-  @IsEmail()
-  @ApiProperty({
-    example: "dev@olivestonelab.com",
-  })
-  adminEmail: string;
-
-  @IsEnum(AdminRole)
-  @ApiProperty({
-    example: AdminRole.Watch,
-  })
-  role?: AdminRole;
-}
+export class AdminCreateInputDto extends PickType(AdminInfoEntity, [
+  "adminId",
+  "adminPw",
+  "adminName",
+  "adminEmail",
+  "role",
+]) {}
 
 export class AdminCreateOutputDto extends CommonOutPut {}
 
-export class AdminMeOutPutDto {
-  @IsString()
-  @ApiProperty({
-    example: "system",
-  })
-  adminId?: string;
-
-  @IsString()
-  @ApiProperty({
-    example: "system",
-  })
-  adminName?: string;
-
-  @IsEmail()
-  @ApiProperty({
-    example: "dev@olivestonelab.com",
-  })
-  adminEmail?: string;
-
+export class AdminMeOutPutDto extends PickType(PartialType(AdminInfoEntity), [
+  "adminId",
+  "adminName",
+  "adminEmail",
+  "role",
+]) {
   @IsString()
   error?: string;
 }
