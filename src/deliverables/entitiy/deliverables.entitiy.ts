c85efa2cc75330c6cbe20f2722856daa1029472d
younghoonKimm <choosey_@naver.com>
2022-02-03 16:09:57 +0900

    [fixed] delverables entity

--- src/deliverables/entitiy/deliverables.entitiy.ts
+++ src/deliverables/entitiy/deliverables.entitiy.ts
@@ -1,9 +1,14 @@
 import { PrimaryGeneratedColumn, Column, Entity } from "typeorm";
 import { PickType, ApiProperty } from "@nestjs/swagger";
 import { CategoryEntity } from "src/management/entities/category.entity";
+import { IsString } from "class-validator";
 
 export class DeliverLists extends PickType(CategoryEntity, ["seqNo"]) {
   @Column()
+  @IsString()
+  parentSeqNo: string;
+
+  @Column("jsonb", { nullable: true })
   checkedName: string[];
 }
 
@@ -15,7 +20,6 @@ export class DeliverablesEntity {
   @Column()
   deliverable: string;
 
-  @Column("jsonb", { nullable: true })
   @ApiProperty()
   deliverablesLists: DeliverLists[];
 }
