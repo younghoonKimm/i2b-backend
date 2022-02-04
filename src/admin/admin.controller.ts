6296c366aa13cde8e71ecbab53a65d78ec834c75
younghoonKimm <choosey_@naver.com>
2022-02-04 18:33:42 +0900

    [add] 통계 count 로직 추가

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -96,9 +96,9 @@ export class AdminController {
   }
 
   @UseGuards(AuthGuard)
-  @Get("/user/check")
+  @Post("/user/check")
   @ApiBearerAuth("bearerAuth")
-  getUserCheck(@Body() adminId: string) {
+  getUserCheck(@Body() { adminId }: any) {
     return this.adminService.checkUser(adminId);
   }
 }
