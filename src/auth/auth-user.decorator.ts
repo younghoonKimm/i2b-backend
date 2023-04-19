f1eb6377e63206bb32b5e68d2cdedde984435758
younghoonKimm <58809170+younghoonKimm@users.noreply.github.com>
2023-04-19 15:01:06 +0900

    Delete src/auth directory

--- src/auth/auth-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-
-export const AdminAuthUser = createParamDecorator(
-  (data: unknown, context: ExecutionContext) => {
-    const [res] = context.getArgs();
-    const token = res.headers.authorization.replace("Bearer ", "");
-    return token;
-  },
-);
